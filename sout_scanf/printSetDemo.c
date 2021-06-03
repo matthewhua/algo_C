6fe781ded16aac7319f7d63fe902bfc4a71f669f
Admin <1229926359@qq.com>
2021-06-03 14:42:11 +0800

    上传

---
+++ sout_scanf/printSetDemo.c
@@ -0,0 +1,15 @@
+//
+// Created by Admin on 2021/6/3.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a1 = 20, a2 = 345, a3 = 700, a4 = 22;
+    int b1 = 56720, b2 = 9999, b3 = 20098, b4 = 2;
+    int c1 = 233, c2 = 205, c3 = 1, c4 = 6666;
+    int d1 = 34, d2 = 0, d3 = 23, d4 = 23006783;
+
+    printf("%-9d %-9d %-9d %-9d\n" ,a1 ,a2, a3, a4 );
+    return 0;
+}
