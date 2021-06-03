77fade0fa8d46a727e7cf8d5ed45c51c022fe320
Admin <1229926359@qq.com>
2021-06-03 15:31:24 +0800

    上传C的语法学习

---
+++ c_learn/getCharDemo.c
@@ -0,0 +1,27 @@
+//
+// Created by Admin on 2021/6/3.
+//
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    /*
+     * 只会输出一个字节
+     * char c;
+    c = getchar();
+    printf("c: %c\n", c);
+    return 0;*/
+
+    /*
+     * 立即输出
+     * char c = getch();
+    printf("c: %c\n", c);
+    return 0;*/
+
+
+    // 输入字符串
+    char author[30], lang[30], url[30];
+
+    printf("author: %s\n", author);
+}
